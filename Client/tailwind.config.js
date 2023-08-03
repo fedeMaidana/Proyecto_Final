b2d8ea67d0c59d452f885def40d52b7a25218825
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-08-03 18:47:22 -0500

    Modificación de archivos de Tailwind, algunos estilos de Intro

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/**/*.{html,js,jsx}'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'principal-black': '#1f1f1f',
+        'principal-white': '#ebebeb',
+        'secondary-violet': '#52489c',
+        'secondary-blue': '#2e4ac5'
+      }
+    },
   },
   plugins: [],
 }

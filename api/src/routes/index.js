c651677996eb364b9e7e95086cbd4207c0101b5f
Mateo-de-la-Torre <mateo.delatorre@gmail.com>
2023-08-03 16:10:48 -0300

    Rutas Usuarios, Register , Login y Borrado logico

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,17 +1,12 @@
 const {Router} = require("express");
-
-const { getUsersHandler, registerHandler, getUserIDHandler, loginHandler, deleteHandler } = require('../handlers/usersHandler');
+const { userRoutes } = require('./userRoutes')
 
 
 
 const mainRouter = Router();
 
 
-mainRouter.get("/users", getUsersHandler);
-mainRouter.get("/users/:id", getUserIDHandler);
-mainRouter.post("/register", registerHandler);
-mainRouter.post("/login", loginHandler);
-mainRouter.put("/deleteuser/:id", deleteHandler);
+mainRouter.use('/', userRoutes);
 
 
 module.exports = mainRouter;

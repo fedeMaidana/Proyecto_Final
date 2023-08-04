8063f7c921c090c76dd697c0846cd99faeb64722
Sacha Figueredo <sacha_figueredo@hotmail.com>
2023-08-03 22:00:05 -0300

    home

--- Client/src/views/Home.jsx
+++ Client/src/views/Home.jsx
@@ -1,9 +1,20 @@
-import { NavLink } from "react-router-dom"
-
+import { NavLink } from "react-router-dom";
 const Home = () => {
     return(
         <>
             <h1>Home</h1>
+            <NavLink to= "/CustomCraft">
+                <button>CustomCraft</button>
+            </NavLink>
+            <NavLink to="/Buy">
+                <button>Buy</button>
+            </NavLink>
+            <NavLink to="/Login">
+                <button>Login</button>
+            </NavLink>
+            <NavLink to="/Search">
+                <button>Search</button>
+            </NavLink>
         </>
     )
 }

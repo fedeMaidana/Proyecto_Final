b142f9c06704b587e277aa283e0e872afc1d92d1
Federico Maidana <fede_maidana4@hotmail.com>
2023-08-06 20:33:57 -0300

    Delete Client directory

--- Client/src/handlers/handlers.js
+++
@@ -1,9 +0,0 @@
-import { setColor, setSize } from "../redux/actions"
-
-export const handleColor = ( color, dispatch ) => {
-    dispatch( setColor( color ) )
-}
-
-export const handleSize = ( size, dispatch ) => {
-    dispatch( setSize( size ) )
-}
--- Client/src/redux/action-types.js
+++
@@ -1,6 +0,0 @@
-export const GET_PRODUCTS = 'GET_PRODUCTS'
-export const DELETE_PRODUCTS = 'DELETE_PRODUCTS'
-export const GET_PRODUCT_DETAIL = 'GET_PRODUCT_DETAIL'
-export const SEARCH_PRODUCT = 'SEARCH_PRODUCT'
-export const SET_COLOR = 'SET_COLOR'
-export const SET_SIZE = 'SET_SIZE'

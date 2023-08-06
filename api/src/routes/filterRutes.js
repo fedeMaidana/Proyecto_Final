eb9f9f07d1b7b01639303fc92c4b3b74df94d6c4
Mateo de la Torre <86978924+Mateo-de-la-Torre@users.noreply.github.com>
2023-08-06 19:21:20 -0300

    Delete api directory

--- api/src/routes/filterRutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const filterRouter = express.Router();
-
-const filter = require('../Filters/filters')
-
-filterRouter.get('/', filter)
-
-
-module.exports = filterRouter;

a9756e33e6855b9bd0a684d6155b09d5f64fa434
Gabinlavin <dcgabriel2017@gmail.com>
2023-08-05 23:56:09 -0300

    mas estilos de social shop

--- Client/src/views/Community.jsx
+++ Client/src/views/Community.jsx
@@ -4,23 +4,59 @@ import { useDispatch, useSelector } from "react-redux"
 import { getProducts } from "../redux/actions"
 
 export function Community () {
-    // const dispatch = useDispatch();
-    // const productos = useSelector((state)=> state.products);
+  // const dispatch = useDispatch();
+  // const productos = useSelector((state)=> state.products);
 
-    // useEffect(() => {
-    //     dispatch(getProducts());
-    //   }, [dispatch]);
+  // useEffect(() => {
+  //     dispatch(getProducts());
+  //   }, [dispatch]);
 
-    return(
-        
+  return (
+    <div>
+      <div className="bg-gray-200 p-4">
         <div className="container mx-auto px-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        <CardsContainer />
+          <div className="flex justify-between">
+            {/* Searchbar */}
+            <div className="bg-white rounded-lg shadow-lg p-4 flex-1 mr-4">
+              <p className="text-xl font-bold mb-2">Encuentra lo que quieres</p>
+              {/* Aquí va el contenido del Searchbar */}
+              {/* ... */}
+              <input
+                type="text"
+                placeholder="Buscar..."
+                className="border border-gray-300 rounded p-2 w-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-gray-200 p-4">
+        <div className="container mx-auto px-4">
+          <div className="flex justify-between">
+            <div className="bg-white rounded-lg shadow-lg p-4 flex-1 mr-4">
+              <p className="text-xl font-bold mb-2">Filtros 1</p>
+            </div>
+
+            <div className="bg-white rounded-lg shadow-lg p-4 flex-1 mr-4">
+              <p className="text-xl font-bold mb-2">Filtros 2</p>
+            </div>
+
+            <div className="bg-white rounded-lg shadow-lg p-4 flex-1">
+              <p className="text-xl font-bold mb-2">Filtros 3</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div className="container mx-auto px-4 mt-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
+          <CardsContainer />
+        </div>
       </div>
     </div>
-     
-        
-    )
+  );
 }
 
 export default Community;

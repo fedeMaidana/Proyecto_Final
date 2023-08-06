f8ce1c8d57d7593ff8cee724e116d017fa3837b9
Gabi <dcgabriel2017@gmail.com>
2023-08-06 16:43:00 -0300

    social shop

--- Client/src/components/CardsContainer.jsx
+++ Client/src/components/CardsContainer.jsx
@@ -17,7 +17,7 @@ const CardsContainer = () => {
     // )
     return(
         <div className="container mx-auto px-4 py-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-96">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-80">
           <Card />
           <Card />
           <Card />

a116d820a63fedc14648c1c35662b70c07d45ff2
4L3J4NDR0w <alejandro7102003@gmail.com>
2023-08-09 17:19:26 -0500

    intento traer la rama develop

---
+++ Client/.vite/deps/react-dom_client.js
@@ -0,0 +1,8 @@
+import {
+  require_client
+} from "./chunk-BFUBRZ55.js";
+import "./chunk-7YS6IJD2.js";
+import "./chunk-67XTWVEJ.js";
+import "./chunk-5WWUZCGV.js";
+export default require_client();
+//# sourceMappingURL=react-dom_client.js.map

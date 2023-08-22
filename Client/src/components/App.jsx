ea0c5444b41b6e076dcadad9706761c37e77523b
Esteban.much <esteban.muchenvicz@gmail.com>
2023-08-21 23:34:11 -0300

    obj

--- Client/src/components/App.jsx
+++ Client/src/components/App.jsx
@@ -37,7 +37,7 @@ export function App() {
         <Route path="/cancel" element={<PaymentCancel/>} />
         <Route path="/success" element={<PaymentSuccess/>} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard" element={<ProfilePage />} />
+        <Route path="/my-profile" element={<ProfilePage />} />
 
 
       </Routes>

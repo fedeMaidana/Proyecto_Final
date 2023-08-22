50678be788d17acfc9c77339e4fd1f1ba0df1891
Esteban.much <esteban.muchenvicz@gmail.com>
2023-08-22 14:53:16 -0300

    url glb

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [ /*WindiCSS(),*/react()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name][extname]'
+      }
+    }
+  },
+  
+  assetsInclude: ['**/*.glb']
+ 
 })
